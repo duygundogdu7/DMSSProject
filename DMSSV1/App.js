d05bfc9584786d02baa1ba37193b533d544a5829
Duygu Gündoğdu <c1411026@student.cankaya.edu.tr>
2019-03-05 13:27:10 +0300

    deneme

--- DMSSV1/App.js
+++ DMSSV1/App.js
@@ -22,7 +22,7 @@ export default class App extends Component<Props> {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Hello to React Native!</Text>
+        <Text style={styles.welcome}>Naber sıla React Native!</Text>
         <Text style={styles.instructions}>To get started, edit App.js</Text>
         <Text style={styles.instructions}>{instructions}</Text>
       </View>
