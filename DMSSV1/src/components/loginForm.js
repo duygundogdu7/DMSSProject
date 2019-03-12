74fc79bbd8e86a3da6bf11d58ba399f17dd3b2f9
syarengunes <gunesalicem@gmail.com>
2019-03-12 16:55:40 +0200

    .

--- DMSSV1/src/components/loginForm.js
+++ DMSSV1/src/components/loginForm.js
@@ -5,67 +5,56 @@ import { Actions } from 'react-native-router-flux';
 import axios from 'axios';
 
 class LoginForm extends Component {
-    state ={
-        email:'',
-        password:'',
-        loginResponse:''
-    }
-    render() {
-        return (
-          <View>
-              <View>
-                  <Input text='Email' inputPlaceHolder='Emailinizi giriniz'
-                  onChangeText={(text) => {
-                        this.setState({
-                            email: text
-                        })
-                  }}
-                  value={this.state.email}/>
-              </View>
-              <View><Input text='Şifreniz' inputPlaceHolder='Şifrenizi giriniz'
-                  onChangeText={(text) => {
-                        this.setState({
-                            password: text
-                        })
-                  }}
-                  secureTextEntry
-                  value={this.state.password}/>
-                  </View>
-              <View style={styles.buttonWrapper}>
-                <Button
-                onPress={() => {
-                    axios({
-                      method: 'post',
-                      url: 'http://172.20.10.9:8086/user',
-                      data: {
-                          email: this.state.email,
-                          password: this.state.password,
-                      }
-<<<<<<< HEAD
-                  }).then((response) => response.json)
-      .then((responseJson) => {
-         console.log(responseJson);
-         this.setState({
-            loginResponse: responseJson.data
-         })
+  state ={
+      email:'',
+      password:'',
+      loginResponse:''
+  }
+  render() {
+      return (
+        <View>
+            <View>
+                <Input text='Email' inputPlaceHolder='Emailinizi giriniz'
+                onChangeText={(text) => {
+                      this.setState({
+                          email: text
+                      })
+                }}
+                value={this.state.email}/>
+            </View>
+            <View><Input text='Şifreniz' inputPlaceHolder='Şifrenizi giriniz'
+                onChangeText={(text) => {
+                      this.setState({
+                          password: text
+                      })
+                }}
+                secureTextEntry
+                value={this.state.password}/>
+                </View>
+            <View style={styles.buttonWrapper}>
+              <Button
+              onPress={() => {
+                  axios({
+                    method: 'post',
+                    url: 'http://172.20.10.9:8086/user',
+                    data: {
+                        email: this.state.email,
+                        password: this.state.password,
+                    }
+                }).then((response) =>
+       {this.setState({
+          loginResponse: response.data
+       })})
 
-=======
-                  }).then((response) => 
-         {this.setState({
-            loginResponse: response.data
-         })})
-         
->>>>>>> 6f61f4d6ff20ec4df8cf5cb5e827d9baab1fb628
-         if(this.state.loginResponse == 1)
-            Actions.main()
-    
-                  }}
-                color='#E87B79' title='Giriş Yap' />
-                  </View>
-             <Text>{this.state.loginResponse}</Text>
-          </View>
-        );
-      }
+       if(this.state.loginResponse== 1)
+          Actions.main()
+
+                }}
+              color='#E87B79' title='Giriş Yap' />
+                </View>
+        </View>
+      );
+    }
 }
 
 const styles = StyleSheet.create({
