88ba9cde81f065f23a247933e15c4f8c097d6ef3
Duygu Gündoğdu <c1411026@student.cankaya.edu.tr>
2019-03-19 16:38:24 +0300

    card: button added

--- DMSSV1/src/components/common/card.js
+++ DMSSV1/src/components/common/card.js
@@ -1,24 +1,35 @@
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, View,Button } from 'react-native';
 
 const Card = (props) => {
   return (
     <View style={styles.cardWrapper}>
       {props.children}
+      <View style={styles.buttonWrapper}>
+         <Button color='#E87B79' title='tamamlandı' />
+        </View>
     </View>
   )
 }
 
 const styles = StyleSheet.create({
   cardWrapper: {
-    height: 50,
+    height: 80,
     margin: 10,
     borderWidth: 1,
     borderRadius: 2,
     borderColor: '#dddddd',
     justifyContent: 'center',
-    alignItems: 'center'
-  }
+  },
+  buttonWrapper: {
+    marginTop: 20,
+    height: 30,
+    borderRadius: 10,
+    justifyContent: 'center',
+    fontSize: 10,
+    alignItems: 'flex-end',
+
+  },
 });
 
 export { Card };
