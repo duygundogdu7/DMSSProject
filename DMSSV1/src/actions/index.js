f08fe77c2e59d8b68a2e612082a3fab2a003205d
Duygu Gündoğdu <c1411026@student.cankaya.edu.tr>
2019-03-26 13:27:43 +0300

    react-native-elements installed

--- DMSSV1/src/actions/index.js
+++ DMSSV1/src/actions/index.js
@@ -10,22 +10,22 @@ const apiUrl = 'http://192.168.43.165:8086/task';
 
 export const fetchAllTasks = () => {
   console.log("fetchTask throwed")
-  return (dispatch) => {
-    return axios.get(apiUrl, {
-      params: {
+  return async (dispatch) => {
+    try {
+      const response = await axios.get(apiUrl, {
+        params: {
           user_id: '1234'
-      }
-    })
-      .then(response => {
-        console.log("action resp")
-        console.log(response)
-        dispatch({
-          type: FETCH_TASKS,
-          payload: response
-        })
-      })
-      .catch(error => {
-        throw(error);
+        }
       });
+      console.log("action resp");
+      console.log(response);
+      dispatch({
+        type: FETCH_TASKS,
+        payload: response
+      });
+    }
+    catch (error) {
+      throw (error);
+    }
   };
 };
