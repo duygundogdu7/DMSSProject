4c19a0d10c2ed2722d561288fb7f66078bf28d63
syarengunes <gunesalicem@gmail.com>
2019-04-02 15:14:55 +0300

    portfo

--- DMSSV1/src/components/portfolio.js
+++ DMSSV1/src/components/portfolio.js
@@ -3,7 +3,14 @@ import { View, Text, Button, FlatList } from 'react-native';
  class Portfolio extends Component {
   render() {
     return (
-     <View></View>
+     <View>
+       <Text>Konum:</Text>
+       <Input></Input>
+       <Text>Bölüm:</Text>
+       <Input></Input>
+       <Text>Fiyat:</Text>
+       <Input></Input>
+     </View>
     )
   }
 }
