e584c78d1c9653f30ac17c63f24b2a8a8e59ac5f
Duygu Gündoğdu <c1411026@student.cankaya.edu.tr>
2019-04-10 12:09:37 +0300

    portfolio input fix

--- DMSSV1/src/components/portfolio.js
+++ DMSSV1/src/components/portfolio.js
@@ -1,15 +1,16 @@
 import React, { Component } from 'react'
 import { View, Text, Button, FlatList } from 'react-native';
+import {Input} from 'react-native-elements';
  class Portfolio extends Component {
   render() {
     return (
      <View>
        <Text>Konum:</Text>
-       <Input></Input>
+       <Input placeholder = "EXAMPLE"/>
        <Text>Bölüm:</Text>
-       <Input></Input>
+       <Input placeholder = "EXAMPLE"/>
        <Text>Fiyat:</Text>
-       <Input></Input>
+       <Input placeholder = "EXAMPLE"/>
        <Text>Example</Text>
      </View>
     )
