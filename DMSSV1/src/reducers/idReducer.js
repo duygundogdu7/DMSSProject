b9cf91356201d72b8d603d55299e3850fa8caf0c
yarenska <silagunes97@gmail.com>
2019-05-08 15:04:54 +0300

    .

--- DMSSV1/src/reducers/idReducer.js
+++ DMSSV1/src/reducers/idReducer.js
@@ -8,7 +8,6 @@ const INITIAL_STATE = {};
 export default (state = INITIAL_STATE, action) => {
   // A switch is used since if more actions are added in the future, it will be easy
   // to be able to handle this in the reducer since we just add another 'case'.
-  console.log("id reducer'a geldin.");
   switch (action.type) {
     case SEND_ID:
       return action.payload;
