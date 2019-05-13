a5fd91d8bfc05efb42baa52e5e8887d6308229c5
Duygu Gündoğdu <c1411026@student.cankaya.edu.tr>
2019-05-13 20:28:25 +0300

    change ip

--- DMSSV1/src/components/newTask.js
+++ DMSSV1/src/components/newTask.js
@@ -20,7 +20,7 @@ onButtonClicked(){
   this.setState({error: '', loading:true})
   axios({
     method: 'post',
-    url: 'http://192.168.43.165:8086/task',
+    url: 'http://192.168.0.12:8086/task',
     data: {
         title: this.state.title,
         date: this.state.date,
